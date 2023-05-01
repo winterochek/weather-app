718c5b393502d1b6487b9786010d16def1268ee6
Pavel <pavlozherebnov@gmail.com>
2023-05-01 18:47:05 +0300

    basic setup

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+import s from './Home.module.scss';
+// interface HomeProps {}
+
+const Home: FC = () => {
+   return <div className={s.container}>Home</div>;
+};
+
+export default Home;
---
+++ src/pages/MonthStats/MonthStats.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+import s from './MonthStats.module.scss';
+// interface HomeProps {}
+
+const MonthStats: FC = () => {
+   return <div className={s.container}>MonthStats</div>;
+};
+
+export default MonthStats;
